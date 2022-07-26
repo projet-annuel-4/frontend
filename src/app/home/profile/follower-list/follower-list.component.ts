ee55930c33903148a362be79997912c9c1626c72
kelyan-9560 <kelyan.bervin@gmail.com>
2022-07-26 12:34:32 +0200

    prod true

--- src/app/home/profile/follower-list/follower-list.component.ts
+++ src/app/home/profile/follower-list/follower-list.component.ts
@@ -21,7 +21,7 @@ export class FollowerListComponent implements OnInit {
 
     console.log("-----------" + this.fromFriendPage);
     console.log("-----------" + localStorage.getItem('friendId'));
-
+ console.log("zqùelmd:;jqlk:vj;?Z%PFML.?ML/")
 
     if(this.fromFriendPage){
       this.followService.getAllFollowers(localStorage.getItem('friendId') as unknown as number).subscribe(followers => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   DOMAIN: 'http://api-getway:8080',
   API_VERSION: 'api/v1',
