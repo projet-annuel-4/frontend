4885a3e9493ed132312dc92e76cc66d68d6e5d7f
HLimam <heithem.limame@gmail.com>
2022-10-20 19:40:53 +0200

    Update parameter request type in file delete methode

--- src/app/_services/project/fileService.ts
+++ src/app/_services/project/fileService.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { TokenStorageService } from '../token/token-storage.service'
-import { Observable } from 'rxjs'
-import { project_file_service } from '../../../environments/environment'
-import { Filess } from '../../_dtos/project/Filess'
-import { CreateFileRequest } from '../../_dtos/project/CreateFileRequest'
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenStorageService } from '../token/token-storage.service';
+import { Observable } from 'rxjs';
+import { project_file_service } from '../../../environments/environment';
+import { Filess } from '../../_dtos/project/Filess';
+import { CreateFileRequest } from '../../_dtos/project/CreateFileRequest';
 
 @Injectable({
   providedIn: 'root',
@@ -15,11 +15,11 @@ export class FileService {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     }),
-  }
+  };
 
   httpOptionsFile = {
     responseType: 'arraybuffer' as 'json',
-  }
+  };
 
   constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {}
 
@@ -27,40 +27,39 @@ export class FileService {
     return this.http.post<Filess>(
       `${project_file_service.BASE_URL}/${projectId}/file/createFile`,
       file
-    )
+    );
   }
 
   getById(branchId: number, fileId: number): Observable<Filess> {
     return this.http.get<Filess>(
       `${project_file_service.BASE_URL}/branch/${branchId}/file/${fileId}/get`,
       this.httpOptions
-    )
+    );
   }
 
   getFileData(projectId: number, fileNameUrl: string): Observable<ArrayBuffer> {
     return this.http.get<any>(
       `${project_file_service.BASE_URL}/${projectId}/file/getFileData?fileNameUrl=${fileNameUrl}`,
       { responseType: 'arraybuffer' as 'json'},
-    )
+    );
   }
 
   getAllFileFromProject(projectId: number): Observable<string[]> {
     return this.http.get<string[]>(
       `${project_file_service.BASE_URL}/${projectId}/file/getTree`,
       this.httpOptions
-    )
+    );
   }
 
   saveFile(projectId: number, fileName: string, data: FormData): Observable<Filess> {
     return this.http.post<Filess>(
-      `${project_file_service.BASE_URL}/${projectId}/file/saveFile?fileNameUrl=${fileName}`,data
-    )
+      `${project_file_service.BASE_URL}/${projectId}/file/saveFile?fileNameUrl=${fileName}`, data
+    );
   }
 
   deleteFile(projectId: number, fileNameUrl: string): Observable<any> {
     return this.http.delete<any>(
-      `${project_file_service.BASE_URL}/${projectId}/file/deleteFile`,
-      {body: {'fileNameUrl': fileNameUrl}}
-    )
+      `${project_file_service.BASE_URL}/${projectId}/file/deleteFile?fileNameUrl=${fileNameUrl}`
+    );
   }
 }
