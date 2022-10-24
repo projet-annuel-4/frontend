5d4e679581c530875404c4b35d03cedc4cffbc6d
kelyan-9560 <kelyan.bervin@gmail.com>
2022-10-25 00:08:07 +0200

    add some hoverEffect

--- src/app/post/post-card/post-card.component.ts
+++ src/app/post/post-card/post-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Post } from '../../_dtos/post/Post'
+import {PostService} from "../../_services/post/post.service";
+import {CodeService} from "../../_services/code_execution/code.service";
 
 @Component({
   selector: 'app-post-card',
@@ -10,7 +12,7 @@ export class PostCardComponent implements OnInit {
   @Input()
   post: Post
 
-  constructor() {}
+  constructor(public codeService: CodeService) {}
 
   ngOnInit(): void {}
 }
