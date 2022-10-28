7a924080f82a3793dbfd0d26226dc7408b2ea3a2
kelyan-9560 <kelyan.bervin@gmail.com>
2022-10-28 23:03:19 +0200

    refacto create group form

--- src/app/group/group-home/group-home.component.ts
+++ src/app/group/group-home/group-home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Group} from '../../_dtos/group/Group';
 import {GroupService} from '../../_services/group/group.service';
-import {NbDialogService} from '@nebular/theme';
+import {NbDialogService, NbGlobalPhysicalPosition, NbToastrService} from '@nebular/theme';
 import {NewGroupComponent} from '../../shared/dialog/new-group.component';
+import {CreateGroupRequest} from "../../_dtos/group/CreateGroupRequest";
+import {User} from "../../_dtos/user/User";
+import {UserService} from "../../_services/user/user.service";
 
 @Component({
   selector: 'app-group-home',
@@ -11,9 +14,19 @@ import {NewGroupComponent} from '../../shared/dialog/new-group.component';
 })
 export class GroupHomeComponent implements OnInit {
 
+  createGroupRequest = new CreateGroupRequest()
+  mail: string
+  members: Set<User> = new Set<User>()
+  positions = NbGlobalPhysicalPosition
+
+
+  togglePopup: string = 'pop-up-none';
+
+
   groups: Group[];
 
-  constructor(private groupService: GroupService, private dialogService: NbDialogService) { }
+  constructor(private groupService: GroupService, private dialogService: NbDialogService,
+              private nbToasterService: NbToastrService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.initUserGroups();
@@ -25,9 +38,77 @@ export class GroupHomeComponent implements OnInit {
     });
   }
 
+  /*
   createGroup() {
     this.dialogService.open(NewGroupComponent).onClose.subscribe(
       () => this.initUserGroups()
     );
   }
+
+   */
+
+  showModal(){
+    if(this.togglePopup == "pop-up-block"){
+      this.togglePopup = 'pop-up-none'
+    } else if(this.togglePopup == "pop-up-none"){
+      this.togglePopup = 'pop-up-block'
+    }
+  }
+
+
+
+/*** For  newGroup pop up***/
+  addMembers() {
+    this.userService.getByEmail(this.mail).subscribe(
+      user => {
+        if (this.members.has(user)) {
+          this.nbToasterService.show(user.email + ' is already add', `Warning`, {
+            position: this.positions.TOP_RIGHT,
+            status: 'warning',
+          })
+          return
+        }
+        this.members.add(user)
+        this.mail = ''
+      },
+      error => {
+        this.nbToasterService.show('User ' + this.mail + ' not found', `Warning`, {
+          position: this.positions.TOP_RIGHT,
+          status: 'warning',
+        })
+      }
+    )
+  }
+
+  createGroup() {
+    if (this.members.size == 0) {
+      this.nbToasterService.show('A group with only you is not very funny', `Warning`, {
+        position: this.positions.TOP_RIGHT,
+        status: 'warning',
+      })
+      return
+    }
+
+    this.memberToCreateGroupRequest()
+
+    this.groupService.create(this.createGroupRequest).subscribe(
+      then => {
+        this.togglePopup = 'pop-up-none'
+        this.initUserGroups();
+      },
+      () => {},
+      () => {});
+  }
+
+  removeMember(memberToDelete: User) {
+    this.members.delete(memberToDelete)
+  }
+
+  memberToCreateGroupRequest() {
+    let ids: Set<number> = new Set<number>()
+    ids.add(this.userService.getProfile().id)
+    this.members.forEach(member => ids.add(member.id))
+    this.createGroupRequest.members = Array.from(ids)
+  }
+
 }
