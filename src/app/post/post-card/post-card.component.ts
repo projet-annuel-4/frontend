9cc69401d30320653d55b9c6d18018e2cd43b66d
HLimam <heithem.limame@gmail.com>
2022-10-30 07:32:26 +0100

    Update tags style

--- src/app/post/post-card/post-card.component.ts
+++ src/app/post/post-card/post-card.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { Post } from '../../_dtos/post/Post'
-import {PostService} from "../../_services/post/post.service";
-import {CodeService} from "../../_services/code_execution/code.service";
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {Post} from '../../_dtos/post/Post';
+import {CodeService} from '../../_services/code_execution/code.service';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostCardComponent implements OnInit {
   @Input()
-  post: Post
+  post: Post;
 
   constructor(public codeService: CodeService) {}
 
