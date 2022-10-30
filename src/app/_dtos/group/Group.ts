211bcc0630325e355ba4cc63420ddaa592877434
kelyan-9560 <kelyan.bervin@gmail.com>
2022-10-30 13:04:54 +0100

    group : user creator can delete group

--- src/app/_dtos/group/Group.ts
+++ src/app/_dtos/group/Group.ts
@@ -2,14 +2,15 @@ import { User } from '../user/User'
 import { Project } from '../project/Project'
 
 export class Group {
-  id: number
-  name: string
-  members: User[]
-  project: Project[]
+  id: number;
+  creatorId: number;
+  name: string;
+  members: User[];
+  project: Project[];
 
   constructor(id: number, name: string, members: User[]) {
-    this.id = id
-    this.name = name
-    this.members = members
+    this.id = id;
+    this.name = name;
+    this.members = members;
   }
 }
--- src/app/group/group-card/group-card.component.ts
+++ src/app/group/group-card/group-card.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Group } from '../../_dtos/group/Group'
 import { Router } from '@angular/router'
+import {GroupService} from "../../_services/group/group.service";
+import {NbGlobalPhysicalPosition, NbToastrService} from "@nebular/theme";
+import {TokenStorageService} from "../../_services/token/token-storage.service";
+import {delay} from "rxjs/operators";
 
 @Component({
   selector: 'app-group-card',
@@ -10,11 +14,32 @@ import { Router } from '@angular/router'
 export class GroupCardComponent implements OnInit {
   @Input() group: Group
 
-  constructor(private router: Router) {}
+  positions = NbGlobalPhysicalPosition;
+
+  constructor(private router: Router,
+              private groupService: GroupService,
+              private nbToasterService: NbToastrService,
+              public tokenStorage: TokenStorageService) {}
 
   ngOnInit(): void {}
 
   goTo(groupId: number): void {
     this.router.navigate(['/group/', groupId]).then()
+  }
+
+  deleteGroup(groupId: number){
+    this.groupService.delete(groupId).subscribe(
+      then => {},
+      error => {},
+      () => {
+        this.nbToasterService.show('Group deleted successfully', `Confirm`, {
+          position: this.positions.TOP_RIGHT,
+          status: 'success',
+        });
+        setTimeout(()=> {
+          window.location.reload();
+        }, 1000);
+      },
+    );
   }
 }
