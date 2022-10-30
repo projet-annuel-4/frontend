4558b27ceee724e251b062412e41e37248af8e9a
HLimam <heithem.limame@gmail.com>
2022-10-30 23:37:35 +0100

    Chamge css to scss

--- src/app/project/project-page/project-page.component.ts
+++ src/app/project/project-page/project-page.component.ts
@@ -20,7 +20,7 @@ declare let monaco: any;
 @Component({
   selector: 'app-project-page',
   templateUrl: './project-page.component.html',
-  styleUrls: ['./project-page.component.css'],
+  styleUrls: ['./project-page.component.scss'],
 })
 export class ProjectPageComponent implements OnInit, OnChanges {
   editorOptions = {theme: 'vs-dark', language: 'python', readonly: true};
